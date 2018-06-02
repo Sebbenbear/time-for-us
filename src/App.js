c6af945d8932334f9e967a0ce1f3556007351a9b
Pauline Kelly <pauline.m.kelly1@gmail.com>
2018-06-02 21:27:47 +1000

    add new svg background to title, change dimensions of header

--- src/App.js
+++ src/App.js
@@ -13,11 +13,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className=".App-title">Time For Us</h1>
+          <br/>
+          <h4 className=".App-intro">Time For Us lets you find the best time to chat to your loved one across countries!</h4>
         </header>
         <Grid>
           <Row className="show-grid">
             <Col md={6}>
-              <TimeZoneInfoPanel title="Melby's Time Zone" timeZone='Australia/Sydney'/>
+              <TimeZoneInfoPanel title="Their Time Zone" timeZone='Australia/Sydney'/>
             </Col>
             <Col md={6}>
               <TimeZoneInfoPanel title="My Time Zone" timeZone='America/New_York'/>
