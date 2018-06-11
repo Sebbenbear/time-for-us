a3eb77341d713bf50cbde2b77062eedde80060a1
Pauline Kelly <pauline.m.kelly1@gmail.com>
2018-06-11 11:42:04 +1000

    deconstruct react bootstrap import

--- src/components/TimeForUsLabel.js
+++ src/components/TimeForUsLabel.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import Alert from 'react-bootstrap/lib/Alert';
+import { Alert } from 'react-bootstrap';
 
 const TimeForUsLabel = (props) => {
 
-    const name = props.name;
-    const nextTimeToChat = props.nextTimeToChat;
-    const status = props.status;
-    const available = props.available;
-    const alertStyle = available ? 'success' : 'warning';
+  const name = props.name;
+  const nextTimeToChat = props.nextTimeToChat;
+  const status = props.status;
+  const available = props.available;
+  const alertStyle = available ? 'success' : 'warning';
 
-    return (
-        <Alert bsStyle={alertStyle}>
-            <strong>{name} {status}!</strong> {!available && <p>Free in {nextTimeToChat} hour{nextTimeToChat !== 1 && 's'}.</p>}
-        </Alert>
-    );
-}
+  return (
+    <Alert bsStyle={alertStyle}>
+      <strong>{name} {status}!</strong> {!available && <p>Free in {nextTimeToChat} hour{nextTimeToChat !== 1 && 's'}.</p>}
+    </Alert>
+  );
+};
 
 export default TimeForUsLabel;
